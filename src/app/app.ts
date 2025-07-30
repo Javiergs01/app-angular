22d94edc504da51962cfd3b7f0643eb76de472bd
Javier Garcia <jagsalex0127@gmail.com>
2025-07-30 17:36:34 -0500

    Creacion del segundo componente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PruebaComponent } from "./prueba-component/prueba-component";
+import { Comp2 } from "./comp2/comp2";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PruebaComponent],
+  imports: [RouterOutlet, PruebaComponent, Comp2],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
