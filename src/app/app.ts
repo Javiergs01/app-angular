51a972ee45625dc12d7740cfbb5764722f81d65e
Javier Garcia <jagsalex0127@gmail.com>
2025-07-30 17:18:28 -0500

    Creacion de nuestro primer componente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PruebaComponent } from "./prueba-component/prueba-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PruebaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
